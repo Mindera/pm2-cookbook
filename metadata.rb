112b468767acb767895cf88ea4179fef35c4d51e
Luís Almeida <luis.almeida@mindera.com>
2017-01-27 16:02:22 +0000

    reverts poise-javscript version to use the original repository instead of the forked one

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'miguel.fonseca@mindera.com'
 license 'MIT'
 description 'Installs/Configures PM2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.7.3'
+version '0.7.4'
 
-depends 'poise-javascript', '~> 1.1.1'
+depends 'poise-javascript', '~> 1.1.0'
 
 supports 'centos', '~> 6.0'
 supports 'redhat', '~> 6.0'
